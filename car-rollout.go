ae983b27a18ca2e0ac61416d5f3dcea40d548b8a
Roland Philippsen <poftwaresatent@gmail.com>
2013-06-13 16:49:11 +0200

    turned car rollout into A* search, nicely illustrates the exponential explosion of the search which is the basic issue with brute force motion planning

--- car-rollout.go
+++ car-rollout.go
@@ -7,10 +7,10 @@ import (
 
 const (
 	AXLEDIST = 1
-	PHMAX = math. Pi/4
-	TRJ_DURATION = 0.1
+	PHMAX = math.Pi/4
+	TRJ_DURATION = 0.5
 	SIM_NSTEPS = 10
-	TREEDEPTH = 5
+	MAXDIST = 20
 )
 
 type state struct {
@@ -19,6 +19,71 @@ type state struct {
 
 type control struct {
 	s, ph float64
+}
+
+type heap_item struct {
+	q state
+	dist, key float64
+}
+
+func insert(heap []heap_item, item heap_item) []heap_item {
+	if nil == heap || 1 >= len(heap) {
+		heap = make([]heap_item, 2) // heap[0] is skipped
+		heap[1] = item
+		return heap
+	}
+	
+	heap = append(heap, item)
+	//bubble_up(heap, len(heap)-1)
+	index := len(heap) - 1
+	parent := index / 2
+	for parent > 0 && heap[index].key < heap[parent].key {
+		heap[index], heap[parent] = heap[parent], heap[index]
+		index = parent
+		parent = index / 2
+	}
+	
+	return heap
+}
+
+// the top item is returned as heap[0] unless the function returns nil
+// in which case the heap was empty to begin with
+func extract(heap []heap_item) []heap_item {
+	if nil == heap || 1 >= len(heap) {
+		return nil
+	}
+	if 2 == len(heap) {
+		heap := []heap_item{ heap[1] }
+		return heap
+	}
+	
+	heap[0] = heap[1]		// remember that heap[0] is skipped
+	heap[1] = heap[len(heap)-1]
+	heap = heap[:len(heap)-1]
+	//bubble_down(heap, 1)
+	index := 1
+	target := 1
+	for {
+		left  := 2 * index
+		right := left + 1
+		if left < len(heap) && heap[left].key < heap[target].key {
+			target = left
+		}
+		if right < len(heap) && heap[right].key < heap[target].key {
+			target = right
+		}
+		if target == index {
+			return heap
+		}
+		heap[index], heap[target] = heap[target], heap[index]
+		index = target
+	}
+	
+	return heap		// never reached though
+}
+
+func empty(heap []heap_item) bool {
+	return nil == heap || 1 >= len(heap)
 }
 
 func fwd(q state, u control) state {
@@ -43,32 +108,50 @@ func integrate(q state, u control, t float64) []state {
 	return trj
 }
 
-type edge struct {
-	q state
-	depth int
-}
-
 func main() {
-	queue := make([]edge, 1)
+	var heap []heap_item
+	heap = insert(heap, heap_item{ state{0, 0, 0}, 0, 0 })
 	u := []control{
 		{ 1.0, -PHMAX/2},
 		{ 1.0,  PHMAX/2},
-		{ 1.0,  0.0},
+		// { 1.0,  0.0},
 		// {-0.5, -PHMAX},
 		// {-0.5,  PHMAX},
 	}
+	goal := state{ 1.0, 1.0, 0.0 }
+	goaldist := 0.5
 	
-	for queue[0].depth <= TREEDEPTH {
-		ee := queue[0]
-		queue = queue[1:]
+outer:
+	for !empty(heap) {
+		heap = extract(heap)
+		ee := heap[0]
+		if ee.dist > MAXDIST {
+			fmt.Println("# max dist reached")
+			break
+		}
 		for _, ut := range(u) {
 			trj := integrate(ee.q, ut, TRJ_DURATION)
-			queue = append(queue, edge{trj[len(trj)-1], ee.depth+1})
-			for _,r := range(trj) {
-				fmt.Printf("% 5f  % 5f  % 5f\n", r.x, r.y, r.th)
+			dist := ee.dist
+////			fmt.Printf("# distance: % 5f  key: % 5f\n", dist, ee.key)
+			for i, r := range(trj) {
+				if i > 0 {
+					dist += math.Sqrt(
+						math.Pow(trj[i-1].x - r.x, 2) +
+						math.Pow(trj[i-1].y - r.y, 2))
+				}
+////				fmt.Printf("% 5f  % 5f  % 5f\n", r.x, r.y, r.th)
 			}
-			fmt.Println()
-			fmt.Println()
+			qend := trj[len(trj)-1]
+			heuristic := math.Sqrt(
+				math.Pow(qend.x - goal.x, 2) + math.Pow(qend.y - goal.y, 2))
+			if heuristic < goaldist {
+				fmt.Println("# goal reached")
+				break outer
+			}
+			heap = insert(heap, heap_item{qend, dist, dist + heuristic})
+			fmt.Printf("% 5f  % 5f  % 5f  % 5f\n", qend.x, qend.y, qend.th, dist)
+////			fmt.Println()
+////			fmt.Println()
 		}
 	}
 }
