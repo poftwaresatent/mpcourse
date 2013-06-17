44d9f1416b8bd7e9ae2c0435f11b257b34dbc35b
Roland Philippsen <poftwaresatent@gmail.com>
2013-06-17 08:30:05 +0200

    trying out json parsing

---
+++ try-json.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type Setup struct {
+	Qstart, Qgoal []float64
+	Environment [][]float64
+}
+
+func main() {
+	blah := Setup {
+		[]float64 { 1, 2, 3, 4 },
+		[]float64 { 5, 6, 7, 8 },
+		[][]float64 {
+			{  0.0, 0.6, 0.5, 0.6 },
+			{  1.3, 1.3, 1.8, 1.3 },
+			{  1.0, 2.2, 1.2, 2.4 },
+			{  1.2, 0.7, 1.3, 0.6 },
+		},
+	}
+	
+	msg, err := json.Marshal(blah)
+	if nil != err {
+		log.Fatal("marshal failed: ", err)
+	}
+	fmt.Printf("marshal ok\n%s\n", msg)
+	
+	var foo Setup
+	msg, err = ioutil.ReadFile("foo.json")
+	if nil != err {
+		log.Fatal(err)
+	}
+	
+	err = json.Unmarshal(msg, &foo)
+	if nil != err {
+		log.Fatal("unmarshaling foo.json: ", err)
+	}
+	fmt.Println("unmarshal ok\n", foo.Qstart, " ", foo.Qgoal, " ", foo.Environment)
+}
